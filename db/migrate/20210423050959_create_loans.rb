412e26b4028a22814fa224f5e39d8c5868c679e4
MNGiit <kmanoorib@gmail.com>
2021-04-23 00:12:04 -0700

    loan has several attributes

--- db/migrate/20210423050959_create_loans.rb
+++ db/migrate/20210423050959_create_loans.rb
@@ -1,6 +1,13 @@
 class CreateLoans < ActiveRecord::Migration[6.1]
   def change
     create_table :loans do |t|
+      t.string :customer_name
+      t.string :insurance_policy
+      t.integer :policy_number
+      t.float :total_coverage_amount
+      t.float :premium_amount
+      t.date :due_date
+      t.date :maturity_date # if team policy and due paid/not paid
 
       t.timestamps
     end

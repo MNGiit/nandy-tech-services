fca56dda20c9c6038f4bbfaec4b9f16b1ab7f8bf
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-04-24 18:08:18 -0700

    #new will check if session already exists

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
 class SessionsController < ApplicationController
+
+  def new
+    if !session[:user_id] # if session doesn't have a user_id
+      @user = User.new
+    else   # else redirect_to another page
+      redirect_to root_path
+    end
+  end
+
 end

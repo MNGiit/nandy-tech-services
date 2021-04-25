8770f7b12814e11420b94875b03b4ee427471d03
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-04-24 18:20:22 -0700

    #create will check if user exists

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,4 +8,15 @@ class SessionsController < ApplicationController
     end
   end
 
+  def create
+    # search for existing user
+    @user = User.find_by(name: params[:user][:name])
+    if @user # implement password check
+      session[:user_id] = @user
+      redirect_to @user # redirect_to user_path(@user)
+    else # user wasn't found
+      redirect_to login_path
+    end
+  end
+
 end

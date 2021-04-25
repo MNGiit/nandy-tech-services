eb310bc64225c722681a8088894f154c62876bd0
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-04-24 22:01:03 -0700

    change route to clear session

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root 'application#home' # index page
+  root 'application#home' # index page
 
   # log in or out
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
+  # delete '/logout' => 'sessions#destroy'
+  get 'logout' => 'sessions#destroy'
 
   # users
   get '/user' => 'users#show'
-
+  get '/users' => 'users#index'
 
   # insurances
   get 'insurances/new', to: 'insurances#new'

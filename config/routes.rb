58c92afe3d37cd6db5fcf6f46e1cd16c096eda4f
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-04-24 17:59:03 -0700

    add 3 routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#home' # index page
+
+  # log in or out
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
+  # insurances
   get 'insurances/new', to: 'insurances#new'
 
-
+  # loans
+  
 end

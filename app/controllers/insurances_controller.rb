835bfc16f26cd9022c150898f7e68c2a0361e177
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-04-25 15:05:09 -0700

    add route to insurances#index

--- app/controllers/insurances_controller.rb
+++ app/controllers/insurances_controller.rb
@@ -1,5 +1,13 @@
 class InsurancesController < ApplicationController
 
+  def index
+    if session[:user_id]
+      @insurances = Insurance.all
+    else
+      redirect_to login_path
+    end
+  end
+
   def new
     render :new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
 
   # insurances
+  get 'insurances', to: 'insurances#index'
   get 'insurances/new', to: 'insurances#new'
 
   # loans

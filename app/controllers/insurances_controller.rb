f7aabc53e90eb4ed349948f62f6abf8e03056c0b
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-04-25 17:19:13 -0700

    create insurance

--- app/controllers/insurances_controller.rb
+++ app/controllers/insurances_controller.rb
@@ -1,5 +1,4 @@
 class InsurancesController < ApplicationController
-
   def index
     if session[:user_id]
       @insurances = Insurance.all
@@ -13,4 +12,22 @@ class InsurancesController < ApplicationController
     render :new
   end
 
+  def create
+    # params[:insurance].merge!(user_id: params[:user_id]) # .merge helps add :user_id to params[:insurance]
+    @insurance = Insurance.create(insurance_params)
+    redirect_to @insurance
+    # @insurance = Insurance.new(insurance_params)
+    # if @insurance.valid?
+    #  @insurance.save
+    #  redirect_to @insurance
+    #  else
+    #  redirect_to :new
+    # end
+  end
+
+  private
+
+  def insurance_params
+    params.require(:insurance).permit(:customer_name, :insurance_policy, :total_coverage_amount, :premium_amount, :due_date, :maturity_date)
+  end
 end

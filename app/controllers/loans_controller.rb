a457c7e29c1fa4147bc00929724948dcc742c2ae
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-04-26 00:47:05 -0700

    #create will create a new loan

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -11,4 +11,15 @@ class LoansController < ApplicationController
     @loan = Loan.new
     render :new
   end
+
+  def create
+    @loan = Loan.create(loan_params)
+    redirect_to @loan
+  end
+
+  private
+
+  def loan_params
+    params.require(:loan).permit(:customer_name, :loan_type, :loan_amount, :pending_principle_amount, :interest_rate, :loan_closed)
+  end
 end
